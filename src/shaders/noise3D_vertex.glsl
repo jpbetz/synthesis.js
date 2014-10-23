8a0cc1e3475a84d3bf79ad5d13b77ec71562713b
Joe Betz <jpbetz@gmail.com>
2014-10-23 13:16:35 -0700

    fix light

--- src/shaders/noise3D_vertex.glsl
+++ src/shaders/noise3D_vertex.glsl
@@ -10,21 +10,30 @@ const float DiffuseContribution = 1.0 - SpecularContribution;
 
 void main()
 {
-  vec3 ecPosition = vec3(modelViewMatrix) * position;
+  // vertex in view space
+  vec4 ecPosition = modelViewMatrix * vec4(position, 1.0);
+  vec3 ecPosition3 = ecPosition.xyz;
+
+  // normal in view space
   vec3 tnorm = normalize(normalMatrix * normal);
-  vec3 viewVec = normalize(-ecPosition);
+
+  // unit vector toward viewer from vector in view space
+  vec3 viewVec = normalize(-ecPosition3);
 
   lightIntensity = 0.0;
   for(int l = 0; l < MAX_POINT_LIGHTS; l++) {
-    vec3 lightPosition = pointLightPosition[l];
+    // light position in view space (it was already in worldspace, so no model transform not applied)
+    vec4 lightPosition = viewMatrix * vec4( pointLightPosition[l], 1.0 );
+    vec3 lightPosition3 = lightPosition.xyz;
+
     vec3 lightColor = pointLightColor[l];
     float maxLightDistance = pointLightDistance[l];
 
-    vec3 positionLightVector = lightPosition-ecPosition;
+    vec3 positionLightVector = lightPosition3-ecPosition3;
 
     float distanceToLight = length(positionLightVector);
 
-    if(distanceToLight <= maxLightDistance) {
+    if(distanceToLight <= maxLightDistance) { // if the light is in range
       //float attenuation = 1.0 / (distanceToLight * distanceToLight);
       vec3 lightVec = normalize(positionLightVector);
       //vec3 reflectVec = reflect(-lightVec, tnorm); // for specular
@@ -34,5 +43,5 @@ void main()
   }
 
   mPosition = normalize(position);
-  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  gl_Position = projectionMatrix * ecPosition;
 }
